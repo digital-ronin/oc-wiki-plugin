7afeb7f7eaf58178ed8b313372d57747ab7f05c9
Daniel-Bruni Ziermann <danielbruni@users.noreply.github.com>
2016-08-26 19:09:22 +0200

    Create lang.php

---
+++ lang/en/lang.php
@@ -0,0 +1,35 @@
+<?php return [
+    'plugin' => [
+        'name' => 'Wiki',
+        'description' => ''
+    ],
+    'wiki' => [
+        'menu_label' => 'Wiki',
+        'menu_description' => 'Manage Wiki Pages',
+        'pages' => 'Pages',
+        'create_page' => 'Wiki page'
+    ],
+    'pages' => [
+        'list_title' => 'Manage the wiki pages',
+        'new_page' => 'New page',
+    ],
+    'page' => [
+        'title' => 'Title',
+        'title_placeholder' => 'New page title',
+        'slug' => 'Slug',
+        'slug_placeholder' => 'new-page-slug',
+        'created' => 'Created',
+        'created_date' => 'Created date',
+        'updated' => 'Updated',
+        'updated_date' => 'Updated date',
+        'delete_confirm' => 'Delete this page?',
+        'close_confirm' => 'The page is not saved.',
+        'return_to_pages' => 'Return to pages list'
+    ],
+    'settings' => [
+        'wiki_title' => 'Wiki',
+        'wiki_description' => 'Display wiki.',
+        'wiki_slug' => 'Wiki Slug',
+        'wiki_slug_description' => 'Look up the wiki page using the supplied slug value.'
+    ]
+];
