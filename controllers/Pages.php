1c8d9b03ad108aba33642c3951e18cc664aa525e
Daniel-Bruni Ziermann <danielbruni@users.noreply.github.com>
2016-08-26 19:13:33 +0200

    Create Pages.php

---
+++ controllers/Pages.php
@@ -0,0 +1,34 @@
+<?php namespace DigitalRonin\Wiki\Controllers;
+
+use BackendMenu;
+use Backend\Classes\Controller;
+
+/**
+ * Pages Back-end Controller
+ */
+class Pages extends Controller
+{
+    public $implement = [
+        'Backend.Behaviors.FormController',
+        'Backend.Behaviors.ListController'
+    ];
+
+    public $formConfig = 'config_form.yaml';
+    public $listConfig = 'config_list.yaml';
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        BackendMenu::setContext('DigitalRonin.Wiki', 'wiki', 'pages');
+    }
+
+    public function create()
+    {
+        BackendMenu::setContextSideMenu('new_page');
+
+        $this->bodyClass = 'compact-container';
+
+        return $this->asExtension('FormController')->create();
+    }
+}
